84d9660875d10c3d45436f6df3971e70243607f3
Vasquezzz247 <00081320@uca.edu.sv>
2025-07-14 16:50:54 -0600

    add: merge route

--- pdf-grinder-api/routes/pdf.js
+++ pdf-grinder-api/routes/pdf.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-// temporal: ruta base
+const upload = require('../middlewares/upload');
+const { mergePDFs } = require('../controllers/pdfController');
+
 router.get('/test', (req, res) => {
     res.send('PDF Grinder API is working!');
 });
+
+router.post('/merge', upload.array('files', 10), mergePDFs);
 
 module.exports = router;
