37b5b07ef73024a848f56a7affc8f192ede3a4df
Vasquezzz247 <00081320@uca.edu.sv>
2025-07-23 17:30:55 -0600

    add: db connection

--- pdf-grinder-api/index.js
+++ pdf-grinder-api/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { connectToDatabase } = require('./config/database');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectToDatabase();
 
 app.use(cors());
 app.use(express.json());
